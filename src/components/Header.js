e8ef373fe01ab5b733e4e1d1eadba5f33e78c6cf
tralalax <timepro@gmx.com>
2023-04-09 22:17:15 +0200

    first commit for V2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,126 +1,148 @@
 import React, { Component } from "react";
-import Typical from "react-typical";
-import Particles from "react-tsparticles";
-import ClipboardJS from "clipboard";
 import Tooltip from '@mui/material/Tooltip';
-import { loadFull } from "tsparticles";
 import data from "./data.json";
+import dataProjects from "./dataProjects.json";
+import dataSkills from "./dataSkills.json";
 import "animate.css/animate.min.css";
 
-new ClipboardJS(".discord-button");
+var name = (data.basic_info.name);
+var sub_name = (data.basic_info.sub_name);
+var github_link = (data.basic_info.github_link);
+var about_title = (data.basic_info.about_title);
+var text_box_content = (data.basic_info.text_box_content);
+var project_title = (data.basic_info.project_title);
+var skills_navbar = (data.basic_info.skills_navbar);
+var github_button = (data.projects.github_button);
+var skills_title = (data.basic_info.skills_title);
+var copyright = (data.basic_info.copyright);
+
+var projects = dataProjects.projects.cards.map(function (project, i) {
+  return (
+      <span className="projects-card-item">
+        <h3>{project.title}</h3>
+        <p>{project.text}</p>
+        <Tooltip title={<h1 className="tooltip-title">{github_button}</h1>}>
+          <a href={project.link} className="projects-card-github-button"></a>
+        </Tooltip>
+      </span>
+  );
+});
+
+var skills = dataSkills.skills.cards.map(function (skill, i) {
+  return (
+      <Tooltip title={<h1 className="tooltip-title">{skill.tooltip}</h1>}>
+        <span className="skills-icon-item">
+          <div className={skill.class}></div>
+          <p>{skill.name}</p>
+        </span>
+      </Tooltip>
+  );
+});
+
 
 class Header extends Component {
-  sub_titles = [];
+
+  componentDidMount() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let interval = null;
+    const h1List = document.querySelectorAll("#home a");
+    
+    h1List.forEach((h1) => {
+      h1.onmouseover = function(event) {  
+        let iteration = 0;
+        clearInterval(interval);
+        
+        interval = setInterval(function() {
+          event.target.textContent = event.target.textContent
+            .split("")
+            .map(function(letter, index) {
+              if (index < iteration) {
+                return event.target.dataset.value[index];
+              }
+              return letters[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+          
+          if (iteration >= event.target.dataset.value.length) { 
+            clearInterval(interval);
+          }
+          
+          iteration += 1 / 3;
+        }, 30);
+      }
+    })
+  }
+
 
   render() {
-    var name = (data.basic_info.name);
-    var github_link = (data.basic_info.github_link);
-    var discord_link = (data.basic_info.discord_link);
-    var youtube_link = (data.basic_info.youtube_link);
-    var titles = (data.basic_info.titles);
-
-    const particlesInit = async (main) => {
-      console.log(main);
-      await loadFull(main);
-    };
-  
-    const particlesLoaded = (container) => {
-      console.log(container);
-    };
-
     return (
-      <header id="home" style={{ height: window.innerHeight, display: 'block' }}>
-        <div id="particles-js"><Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: false,
-                mode: "push"
-              },
-              onHover: {
-                enable: false,
-                mode: "repulse"
-              },
-              resize: true
-            },
-            modes: {
-              push: {
-                quantity: 4
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4
-              }
-            }
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-              opacity: 0.1,
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.1,
-              width: 1
-            },
-            collisions: {
-              enable: true
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce"
-              },
-              random: false,
-              speed: 0.5,
-              straight: false
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800
-              },
-              value: 80
-            },
-            opacity: {
-              value: 0.5
-            },
-            shape: {
-              type: "circle"
-            },
-            size: {
-              value: { min: 1, max: 5 }
-            }
-          },
-          detectRetina: true
-        }}
-      /></div>
-        <div className="header-aligner" style={{height: '100%'}}>
-          <div>
-            <h1>
-              <Typical steps={[name]}/>
-            </h1>
-            <div className="title-container">
-              <Typical className="title-styles" steps={titles.map(x => [ x, 1500 ] ).flat()} loop={50}/>
+      <header id="home" style={{height: window.innerHeight}}>
+
+        <div className="nav-bar">
+          <div className="nav-bar-center-section">
+            <a data-value={skills_navbar} href="#skills" >{skills_navbar}</a>
+          </div>
+          <div className="nav-bar-center-section">
+            <a data-value={project_title} href="#projects" >{project_title}</a>
+          </div>
+        </div>
+
+        <div className="header-aligner">
+          <div className="name-title">
+            <div className="name-section">
+              <h1>{name}</h1>
             </div>
-            <div className="social-link">
-              <Tooltip arrow title={<h1 className="tooltip-title">Mon Github</h1>}>
-                <a href={github_link} className="github-button"></a>
-              </Tooltip>
-              <Tooltip arrow title={<h1 className="tooltip-title">Copié dans le presse-papiers</h1>}>
-                <button className="discord-button" data-clipboard-text={discord_link}></button>
-              </Tooltip>
+            <div className="sub-name-section">
+              <h1>{sub_name}</h1>
+            </div>
+          </div>
+
+          <div className="about-me-section">
+            <div className="text-box">
+              <div className="text-box-title">
+                <h1>{about_title}</h1>
+              </div>
+              <div className="text-box-content">
+                <p>{text_box_content}</p>
+              </div>
             </div>
           </div>
         </div>
+
+        <div className="social-link">
+          <div className="github-link-box">
+            <div className="github-profile-card">
+              {/* TODO */}
+            </div>
+          </div>
+          <div className="github-stats-box">
+            <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=tralalax&langs_count=8&layout=compact&theme=rose_pine"></img>
+          </div>
+        </div>
+
+        <section id="projects">
+            <div className="projects-title">
+              <h1>{project_title}</h1>
+            </div>
+            <div className="projects-card-box">
+              {projects}
+            </div>
+        </section>
+
+        <section id="skills">
+            <div className="skills-title">
+              <h1>{skills_title}</h1>
+            </div>
+
+          <div className="skills-icon-box">
+            {skills}
+          </div>
+        </section>
+
+        <footer>
+          <div className="footer-copyright">{copyright}</div>
+        </footer>
+
       </header>
     );
   }
