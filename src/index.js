83f0a4d3bc64c67fd7f044f7cb91e1c95285a39a
tralalax <timepro@gmx.com>
2023-10-29 16:30:20 +0100

    trying to fix dis shit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter, Route, Link } from "react-router-dom";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <React.StrictMode>
+//         <HashRouter>
+//             <App/>
+//         </HashRouter>
+//     </React.StrictMode>,
+// )
